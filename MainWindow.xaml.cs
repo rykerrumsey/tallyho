423b0d2b909d5032c54a65e2ad29a62468a4479b
Ryker Rumsey <rykerrumsey@outlook.com>
2018-06-06 23:13:50 -0600

    Added mvvm layout to tab icons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using tallyho.ViewModels;
 
 namespace tallyho
 {
@@ -23,6 +24,43 @@ namespace tallyho
         public MainWindow()
         {
             InitializeComponent();
+
+            // create new well dialog
+
+            // search for well
+            // view well info
+            // casing tally view
+            // settings window
+
+            // import .json file
+
+
+            // search database for well, date
+            // recent wells that were opened
+
+            // select all casing data
+            // select all string data
+
+
+
+            // add well to database
+            // add string to database
+            // add casing to database
+
+            // delete well from database
+            // delete string from database
+            // delete casing from database
+
+
+            // add string to well
+            // verify string add dialog
+
+            // notes: todo: 
+            // - casing visualization
+            // - editable strings
+            // - editbale wells
+            // - printing wells
+            // - more casing type info
 
             using (var db = new TallyHoDb())
             {                
@@ -39,5 +77,26 @@ namespace tallyho
                 db.Dispose();
             }
         }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            DataContext = new SearchViewModel();
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            DataContext = new InfoViewModel();
+        }
+
+        private void btnTally_Click(object sender, EventArgs e)
+        {
+            DataContext = new TallyViewModel();
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            DataContext = new SettingsViewModel();
+        }
+
     }
 }
